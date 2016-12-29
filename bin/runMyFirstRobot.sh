365f8a5a324580ca8a9fc20e4c9d61cc2ab74ff3
martinfou <martinfou@gmail.com>
2016-12-29 13:27:24 -0500

    adding data and modification to my new robot

---
+++ bin/runMyFirstRobot.sh
@@ -0,0 +1,2 @@
+cd "/Applications/IC Markets.app/drive_c/Program Files/MetaTrader 4 IC Markets/"
+/usr/local/bin/wine "/Applications/IC Markets.app/drive_c/Program Files/MetaTrader 4 IC Markets/terminal.exe" "./MQL4/bin/myFirstRobot.ini"
